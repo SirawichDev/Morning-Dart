7eac588dc4f47a61600459d2cae3dfe54ae34656
SirawichDev <sirawit0676@gmail.com>
2018-11-17 10:12:41 +0700

    print variable in

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,7 +1,15 @@
 void main(){
   var a = "miew";
   var b = "miew miew";
-
+//Type
+  String name;
+  name = "sirawich";
+  print(name);
+  num number = 22;
+  print(number);
   print(a+b);
+  String n = "sirawich";
+  int age = 23;
+  print("my name is $n i'm $age agoo ");
 }
 
