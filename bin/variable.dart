5c1794382700e7b37320a905c30d150ab438353f
SirawichDev <sirawit0676@gmail.com>
2018-11-17 10:03:59 +0700

    variable declare 💾

---
+++ bin/variable.dart
@@ -0,0 +1,7 @@
+void main(){
+  var a = "miew";
+  var b = "miew miew";
+
+  print(a+b);
+}
+
---
+++ test/Morning_Dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:Morning_Dart/Morning_Dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
